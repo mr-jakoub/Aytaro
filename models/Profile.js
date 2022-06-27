41ca768d3ac27b784fb78a7edd5159292a29fe28
mr-jakoub <jakoubinfo@gmail.com>
2022-06-27 19:50:28 +0100

    Create Profile model

---
+++ models/Profile.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const ProfileSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    validated:{
+        type: Boolean,
+        default: false
+    },
+    rank:{
+        type: String,
+        default: "Bronze"
+    },
+    courses:[
+        {
+            course:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Course"
+            }
+        }
+    ],
+    followers:[
+        {
+            user:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        }
+    ],
+    rises:[
+        {
+            user:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            Ntime:{
+                type: Number
+            }
+        }
+    ],
+    location:{
+        type: String
+    },
+    bio:{
+        type: String
+    },
+    skills:{
+        type: [String]
+    }
+})
+
+module.exports = User = mongoose.model('Profile',ProfileSchema)
