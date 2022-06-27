8dd38aae2bbf07daeb15d3bf89876c590f6c5789
mr-jakoub <jakoubinfo@gmail.com>
2022-06-27 17:46:40 +0100

    Add a jwt token after registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,9 +12,53 @@ router.post('/', [
     check('email','Please include a valid email address').isEmail(),
     check('password','Password must be at least 6 characters').isLength({ min: 6 }),
     check('gender','Gender is required').not().isEmpty(),
-    check('birthdate','Please include a valid birthdate').isDate({format: 'DD-MM-YYYY'})
+    check('birthdate','Please include a valid birthdate').isDate({format: 'YYYY-MM-DD'})
 ], async (req,res) =>{
-    
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    let { accountType, firstname, lastname, email, phone, password, gender, birthdate } = req.body
+    try {
+        // Check if user exists
+        let user = await User.findOne({ email })
+        if (user) {
+            return res.status(400).json({ errors: [{msg: `Hey ${user.firstname} 😃 you are already one of us`}] })
+        }
+        
+        // Get users avatar
+        const avatar = "default"
+        user = new User({
+            accountType,
+            firstname,
+            lastname,
+            email,
+            password,
+            gender,
+            phone,
+            avatar,
+            birthdate
+        })
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
+        await user.save()
+        
+        // Return JWT
+        const payload = {
+            user:{
+                id: user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'), {expiresIn : config.get('jwtExpiresIn')}, (err, token)=>{
+            if (err) throw err
+            res.json({ token })
+        })
+    }catch(err){
+        console.log(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 // @route   GET api/users/recent
